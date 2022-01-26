6e858db638c4a05ae21d31f5791ca0ce4a77a56d
purohitpriyanka <98452280+purohitpriyanka@users.noreply.github.com>
2022-01-26 15:21:34 +0530

    Create ForTest.php

---
+++ Model/ForTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Basic\Module\Model;
+
+/**
+ * Class ForTest
+ *
+ * @package Basic\Module\Model
+ */
+class ForTest
+{
+    public function getContent()
+    {
+
+        $html = "<br> For Test Plugin Working !<br>";
+        return $html;
+        
+    }
+}
